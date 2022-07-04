33cb37f57857eeb035db61c3b26f1561a988fb34
Shreyash <shreyashkumarshrivastava7999@gmail.com>
2022-07-05 01:58:02 +0530

    first commit

---
+++ Shreyash_Shrivastava_0208CS191128.py
@@ -0,0 +1,43 @@
+# Shreyash Kumar Shrivastava
+# 0208CS191128
+# shreyashkumarshrivastava7999@gmail.com
+# Gyan Ganga College of Technology
+
+# Project for AICTE cisco virtual internship 2022
+Info=[]
+def DtH(n):
+    return hex(int(n))
+def DtO(n):
+    return oct(int(n))
+def DtB(n):
+    return bin(int(n)).replace("0b","")
+for i in range(10):
+    IP=input("Enter IPv4 address")
+    IP=IP.split(".")
+    if(len(IP)!=4):
+        print("Invalid IP address")
+        continue
+    for i in IP:
+        if(int(i)<0 or int(i)>255):
+            print("Invalid IP address")
+            break
+    else:
+        IPd=".".join(IP)
+        IPb=list(map(DtB,IP))
+        IPb=".".join(IPb)
+        IPo=list(map(DtO,IP))
+        IPo=".".join(IPo)
+        IPh=list(map(DtH,IP))
+        IPh=".".join(IPh)
+        Info=[IPd,IPb,IPo,IPh]
+        with open("conversion.txt","a+") as f:
+            f.write(str(Info)+"\n")
+        Info=[]
+
+with open("conversion.txt","r") as f:
+    OP=f.readlines()
+    for i in range(10):
+        print("The "+i+1+" IP address in Decimal Binary Octal and Hexadecimal is"+str(OP[i]))
+            
+        
+        
